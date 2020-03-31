f2db50ced3c703ce5dafab8ba2f6e2d0f2ff3e81
Juliana Cardoso <juliana.cardoso@nextel.com.br>
2020-03-31 20:11:44 -0300

    Laço de repetição

--- adivinha.py
+++ adivinha.py
@@ -1,20 +1,29 @@
-print("Bem-vindo ao jogo de adivinhação")
-
+total_rodadas = 3
+rodada_atual = 1
 numero_secreto = 42
 
-chute_str = input("Digite seu chute: ")
-chute = int(chute_str)
+print("Bem-vindo ao jogo de adivinhação")
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+while (rodada_atual <= total_rodadas):
+    print("Você terá ", total_rodadas, "chances, e essa é sua ", rodada_atual, "ª rodada. Boa sorte!")
+   
+    chute_str = input("Digite seu chute: ")
+    
+    chute = int(chute_str)
 
-if (acertou):
-    print("Parabéns, você acertou")
-else:
-    if (maior):
-        print("Que pena, você chutou um número maior!")
-    elif (menor):
-        print("Que pena, você chutou um número menor!")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    rodada_atual = rodada_atual + 11   
+
+    if (acertou):
+        print("Parabéns, você acertou")
+    else:
+        if (maior):
+            print("Que pena, você chutou um número maior!")
+        elif (menor):
+            print("Que pena, você chutou um número menor!")
+
+
 
 print("Fim do jogo!")
