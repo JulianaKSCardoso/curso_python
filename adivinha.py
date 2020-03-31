e3966f5d3b06c230dd1b2ee842b66428e6b08634
Juliana Cardoso <juliana.cardoso@nextel.com.br>
2020-03-31 20:24:40 -0300

    Exercício Adivinhação

--- adivinha.py
+++ adivinha.py
@@ -5,16 +5,16 @@ numero_secreto = 42
 print("Bem-vindo ao jogo de adivinhação")
 
 while (rodada_atual <= total_rodadas):
-    print("Você terá ", total_rodadas, "chances, e essa é sua ", rodada_atual, "ª rodada. Boa sorte!")
+    print("Você terá {} chances, e essa é sua {}ª rodada. Boa sorte!" .format(total_rodadas, rodada_atual))
    
     chute_str = input("Digite seu chute: ")
-    
+
     chute = int(chute_str)
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
-    rodada_atual = rodada_atual + 11   
+    rodada_atual = rodada_atual + 1   
 
     if (acertou):
         print("Parabéns, você acertou")
